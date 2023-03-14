88181b19a98e74c4236ca9bc2a31518d3b3c4b48
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-03-14 17:11:49 -0300

    Atualizado projetoGamer com JS

---
+++ projetoGamer/js/script.js
@@ -0,0 +1,7 @@
+function mostrarMenu() {
+  document.getElementById("menu").classList.toggle("mostrar-menu");
+}
+
+function mostrarOptions() {
+  document.getElementById("options").classList.toggle("mostrar-options");
+}
