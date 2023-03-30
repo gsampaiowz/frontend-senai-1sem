9d625410b0c580290f2fbd26ece272dbda813174
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-03-30 17:24:48 -0300

    exercicios fixacao organizados

---
+++ C#_cSharp/exercicios/exercicios-fixacao/1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Faça um programa que o usuário informe o salário recebido e o total gasto. Deverá ser exibido na tela “Gastos dentro do orçamento” caso o valor gasto não ultrapasse o valor do salário e “Orçamento estourado” se o valor gasto ultrapassar o valor do salário.
+
+//? solicita e guarda salario, solicita e guarda totalGasto / se totalGasto for maior que salario, exibir "Orçamento estourado", já se for menor ou igual, exibir "Gastos dentro do orçamento"
