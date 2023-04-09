90c189d86b53588574297224790803f9b72fc2bf
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-04-09 17:43:50 -0300

    exercicio fibonacci

---
+++ C#_cSharp/exercicios/exercicios-repeticao/extra/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+***********DESAFIO************
+
+ Pesquise como funciona o algoritmo Fibonacci.
+ Faça um programa que gere a série até que o valor seja maior que 500. 
+
+*Algoritmo
+numeroAnterior = 0;
+for numero = 1; numero <= 500; numero += anterior;
+    escreva numero;
+    numeroAnterior = numero;
+*/
+
+//? INICIO
+
+int numeroAntesAnterior = 0;
+int numeroAnterior = 0;
+
+Console.WriteLine($"Digite o valor máximo para essa sequência de Fibonacci: ");
+int numeroMaximo = int.Parse(Console.ReadLine()!);
+
+for (int numero = 1; numero <= numeroMaximo; numero = numeroAntesAnterior + numeroAnterior)
+{
+    Console.Write($"{numero} => ");
+    numeroAntesAnterior = numeroAnterior;
+    numeroAnterior = numero;
+}
