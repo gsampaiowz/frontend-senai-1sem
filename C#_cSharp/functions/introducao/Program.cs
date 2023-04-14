e4c50195bec01d701cdd55eb4d61c8269dd3af8c
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-04-14 17:15:34 -0300

    aaaa explodi

--- C#_cSharp/functions/introducao/Program.cs
+++ C#_cSharp/functions/introducao/Program.cs
@@ -37,3 +37,13 @@
 
 // string texto = ($"{BoaNoite()[0]} {BoaNoite()[1]}");
 // Console.WriteLine(texto);
+
+int[] numeros = new int[] {999,2,1,458,54};
+                        //  0  1 2  3  4
+Console.WriteLine($"{numeros.GetValue(3)}");
+
+string[] nomes = new string[] {"Carlos","Edu","EU"};
+                            //     0      1
+Console.WriteLine($"{nomes.Max()}");
+
+//? .Max  .Min   .Contains   .GetValue
