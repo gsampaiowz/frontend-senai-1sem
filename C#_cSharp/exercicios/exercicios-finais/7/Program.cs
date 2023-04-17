1a0fc5fcdd14fbd7c51f63e94c3df2f2f59355d6
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-04-17 17:26:27 -0300

    exercicios-finais

---
+++ C#_cSharp/exercicios/exercicios-finais/7/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//* 7 - Faça um algoritmo para ler 15 números e armazenar em um vetor. Após a leitura total dos
+//* 15 números, o algoritmo deve escrever esses 15 números lidos na ordem inversa da qual foi
+//* declarado.
+
+float[] numeros = new float[3];
+
+for (int i = 0; i < numeros.GetLength(0); i++)
+{
+    Console.WriteLine($"\nDigite o {i + 1}º número: ");
+    numeros[i] = float.Parse(Console.ReadLine()!);
+}
+
+for (int x = numeros.GetLength(0) - 1; x >= 0; x--)
+{
+    Console.Write($"{numeros[x]}, ");
+}
