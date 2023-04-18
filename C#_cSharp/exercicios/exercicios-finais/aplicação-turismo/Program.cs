7a684a4e7e513f314873ea7ee625de3aa7141f67
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-04-18 17:25:27 -0300

    correcao-aplicacao

--- C#_cSharp/exercicios/exercicios-finais/aplicação-turismo/Program.cs
+++ C#_cSharp/exercicios/exercicios-finais/aplicação-turismo/Program.cs
@@ -1,26 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 string[] nomePassageiros = new string[2];
-string[] origem = new string[5];
-string[] destino = new string[5];
-string[] data = new string[5];
+string[] origens = new string[5];
+string[] destinos = new string[5];
+string[] datas = new string[5];
 
 Console.WriteLine($"\nAgência de Turismo");
 
-// Console.WriteLine($"\nDigite a senha de acesso: ");
-// string senha = Console.ReadLine()!;
+Console.WriteLine($"\nDigite o nome de usuário: ");
+string nomeUsuario = Console.ReadLine()!;
 
-// while (senha != "haaland")
-// {
-//     Console.WriteLine($"\nSenha incorreta!");
-//     senha = Console.ReadLine()!;
-// }
+while (nomeUsuario != "dev")
+{
+    Console.WriteLine($"Nome de usuário incorreto!");
+    nomeUsuario = Console.ReadLine()!;
+}
 
-// Console.WriteLine($"\nCarregando...");
-// Thread.Sleep(3000);
+Console.WriteLine($"\nDigite a senha de acesso: ");
+string senha = Console.ReadLine()!;
 
-// Console.WriteLine($"\nAcesso bem sucedido!");
+while (senha != "senai134")
+{
+    Console.WriteLine($"\nSenha incorreta!");
+    senha = Console.ReadLine()!;
+}
+Console.WriteLine($"\nCarregando...");
+Thread.Sleep(1000);
 
+Console.WriteLine($"\nAcesso bem sucedido!");
 
 bool voltarMenu;
 int escolha = 0;
@@ -32,7 +39,7 @@ do
 _________________________
 |          Menu         |
 |-----------------------|
-| 1. Cadastrar passagem | 
+| 1. Cadastrar passagens| 
 | 2. Listar passagens   |
 | 0. Sair               | 
 -------------------------");
@@ -42,34 +49,51 @@ _________________________
     switch (escolha)
     {
         case 1:
-            for (var i = 0; i < nomePassageiros.Length; i++)
+            char cadastroNovamente;
+            do
             {
-                Console.WriteLine($"\nDigite o nome do {i + 1}º passageiro: ");
-                nomePassageiros[i] = Console.ReadLine()!;
-                Console.WriteLine($"\nDigite a origem do {i + 1}º passageiro: ");
-                origem[i] = Console.ReadLine()!;
-                Console.WriteLine($"\nDigite o destino do {i + 1}º passageiro: ");
-                destino[i] = Console.ReadLine()!;
-                Console.WriteLine($"\nDigite a data da viagem do {i + 1}º passageiro: Exemplo: 26/05/2023");
-                data[i] = Console.ReadLine()!;
-            }
+                for (var i = 0; i < nomePassageiros.Length; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"\nDigite o nome do {i + 1}º passageiro: ");
+                    nomePassageiros[i] = Console.ReadLine()!;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"\nDigite a origem do {i + 1}º passageiro: ");
+                    origens[i] = Console.ReadLine()!;
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine($"\nDigite o destino do {i + 1}º passageiro: ");
+                    destinos[i] = Console.ReadLine()!;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"\nDigite a data da viagem do {i + 1}º passageiro: Exemplo: 26/05/2023");
+                    datas[i] = Console.ReadLine()!;
+                    Console.ResetColor();
+                }
+                Console.WriteLine($"\nGostaria de cadastrar novas passagens? S/N");
+                cadastroNovamente = char.Parse(Console.ReadLine()!.ToUpper());
+            } while (cadastroNovamente == 'S');
             voltarMenu = true;
             break;
         case 2:
+
             for (var i = 0; i < nomePassageiros.Length; i++)
             {
-                Console.WriteLine($"\n O nome do {i + 1}º passageiro: {nomePassageiros[i]}");
-                Console.WriteLine($"\n A origem do {i + 1}º passageiro: {origem[i]}");
-                Console.WriteLine($"\n O destino do {i + 1}º passageiro: {destino[i]}");
-                Console.WriteLine($"\n A data da viagem do {i + 1}º passageiro: {data[i]}");
-                Thread.Sleep(1500);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"\nNome do {i + 1}º passageiro: {nomePassageiros[i]}");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"\nOrigem do {i + 1}º passageiro: {origens[i]}");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($"\nDestino do {i + 1}º passageiro: {destinos[i]}");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"\nData da viagem do {i + 1}º passageiro: {datas[i]}");
+                Console.ResetColor();
+                Thread.Sleep(1000);
             }
             voltarMenu = true;
             break;
         case 0:
-            // Console.WriteLine($"\nFinalizando...");
-            // Thread.Sleep(3000);
-            Environment.Exit(0);
+            Console.WriteLine($"\nEncerrando...");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Programa encerrado.");
             break;
         default:
             Console.WriteLine($"Opção Inválida!");
---
+++ C#_cSharp/exercicios/exercicios-finais/correcao-aplicacao/Program.cs
@@ -0,0 +1,103 @@
+﻿
+
+//* criando as funções
+
+//* estrutura de uma função
+
+//tipo tipoDado nomeFuncao(parâmetros ou argumentos)
+{
+    //corpo da função
+}
+
+static bool Login(string senha)
+{
+    if (senha == "123456")
+    {
+        Console.WriteLine($"Carregando...");
+        Thread.Sleep(3000);
+        Console.WriteLine($"\nAcesso bem sucedido!");
+
+        return true;
+    }
+    else
+    {
+        Console.WriteLine($"\nSenha incorreta.");
+
+        return false;
+    }
+}
+
+//* declarar as variáveis
+string[] nomes = new string[2];
+string[] origens = new string[2];
+string[] destinos = new string[2];
+string[] datas = new string[2];
+
+bool senhaValida;
+do
+{
+    Console.WriteLine($"\nInforme a sua senha: ");
+    string senha = Console.ReadLine()!;
+    senhaValida = Login(senha);
+
+} while (senhaValida == false);
+
+//* criar menu de opções
+string opcao;
+do
+{
+    Console.WriteLine($"\nMenu de opções");
+    Console.WriteLine(@$"
+Selecione uma das opções
+[1] - Cadastrar
+[2] - Listar
+[0] - Sair
+");
+    opcao = Console.ReadLine()!;
+
+    switch (opcao)
+    {
+        case "1":
+            string maisPassagem;
+            do
+            {
+                for (var i = 0; i < nomes.Length; i++)
+                {
+                    Console.WriteLine($"\nInforme o nome do {i + 1}º: ");
+                    nomes[i] = Console.ReadLine()!;
+                    Console.WriteLine($"\nInforme a origem do {i + 1}º: ");
+                    origens[i] = Console.ReadLine()!;
+                    Console.WriteLine($"\nInforme o destino do {i + 1}º: ");
+                    destinos[i] = Console.ReadLine()!;
+                    Console.WriteLine($"\nInforme a data do {i + 1}º: ");
+                    datas[i] = Console.ReadLine()!;
+                }
+                Console.WriteLine($"\nDeseja cadastrar mais passagens? s/n");
+                maisPassagem = Console.ReadLine()!.ToLower();
+
+            } while (maisPassagem == "s");
+            break;
+        case "2":
+            for (var i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine(@$"
+            *******************
+            Passagens - Bilhete {i + 1}
+            Nome: {nomes[i]}
+            Origem: {origens[i]}
+            Destino: {destinos[i]}
+            Data: {datas[i]}");
+                Thread.Sleep(3000);
+            }
+            break;
+        case "0":
+            Console.WriteLine($"Encerrando...");
+            Thread.Sleep(3000);
+            Console.WriteLine($"\nPrograma encerrado.");
+            break;
+
+        default:
+            Console.WriteLine($"\nOpção inválida!");
+            break;
+    }
+} while (opcao != "0");
