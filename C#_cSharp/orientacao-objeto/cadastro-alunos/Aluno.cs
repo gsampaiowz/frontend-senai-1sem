32f7c667cb47ec7afc04f3f732ea78db3dcfe8b8
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-04-24 20:38:17 -0300

    aplicação cadastro de alunos finalizada

--- C#_cSharp/orientacao-objeto/cadastro-alunos/Aluno.cs
+++ C#_cSharp/orientacao-objeto/cadastro-alunos/Aluno.cs
@@ -2,10 +2,10 @@ namespace cadastro_alunos
 {
     public class Aluno
     {
-        public string nome;
-        public string curso;
+        public string nome = string.Empty;
+        public string curso = string.Empty;
         public int idade;
-        public string rg;
+        public string rg = string.Empty;
         public bool bolsista;
         public float mediaFinal;
         public float valorMensalidade;
@@ -13,12 +13,10 @@ namespace cadastro_alunos
         public void VerMediaFinal()
         {
             Console.WriteLine($"\nO valor da média final é: {mediaFinal}");
-
         }
         public void VerMensalidade()
         {
             Console.WriteLine($"\nO valor da mensalidade é: {valorMensalidade}");
-
         }
     }
 }
--- C#_cSharp/orientacao-objeto/cadastro-alunos/Program.cs
+++ C#_cSharp/orientacao-objeto/cadastro-alunos/Program.cs
@@ -25,33 +25,56 @@ string validarBolsa = Console.ReadLine()!.ToLower();
 
 if (validarBolsa == "s") { aluno.bolsista = true; } else { aluno.bolsista = false; }
 
-Console.WriteLine($"\nDigite a média final do aluno: ");
+Console.WriteLine($"\nDigite a média final do aluno: (de 0 a 10)");
 aluno.mediaFinal = float.Parse(Console.ReadLine()!);
-Console.WriteLine($"\nDigite o valor da mensalidade do aluno: ");
+
+Console.WriteLine($"\nDigite o valor da mensalidade do aluno: (valor positivo)");
 aluno.valorMensalidade = float.Parse(Console.ReadLine()!);
 
-Console.WriteLine(@$"
+float desconto = 0;
+if (aluno.bolsista == true)
+{
+    if (aluno.mediaFinal >= 8)
+    {
+        desconto = (float)50 / 100;
+    }
+    else if (aluno.mediaFinal > 6 && aluno.mediaFinal < 8)
+    {
+        desconto = (float)30 / 100;
+    }
+}
+float valorDescontado = aluno.valorMensalidade * desconto;
+aluno.valorMensalidade -= valorDescontado;
+
+string opcao;
+do
+{
+    Console.WriteLine(@$"
 MENU
 [1] - Visualizar a média
 [2] - Visualizar a mensalidade
-[0] - Sair da aplicação");
-string opcao = Console.ReadLine()!;
+[0] - Sair da aplicação
+----------------------------------");
+    opcao = Console.ReadLine()!;
 
-switch (opcao)
-{
-    case "1":
-        aluno.VerMediaFinal();
-        break;
-    case "2":
-        aluno.VerMensalidade();
-        break;
-    case "0":
-        Console.WriteLine($"/nEncerrando...");
-        Thread.Sleep(2000);
-        Console.WriteLine($"/nAplicação encerrada.");
-        break;
-    default:
-        Console.WriteLine($"\nOpção inválida!");
-        break;
-}
+    switch (opcao)
+    {
+        case "1":
+            aluno.VerMediaFinal();
+            break;
+        case "2":
 
+            aluno.VerMensalidade();
+            break;
+        case "0":
+            Console.WriteLine($"\nEncerrando...");
+            Thread.Sleep(2000);
+            Console.WriteLine($"\nAplicação encerrada.");
+            break;
+        default:
+            Console.WriteLine($"\nOpção inválida!");
+            break;
+    }
+} while (opcao != "0");
+
+Console.ResetColor();
