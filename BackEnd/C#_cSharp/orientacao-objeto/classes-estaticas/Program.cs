29efd310fa6e771146dce27f2611e24b0c4bc402
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-05-09 15:10:30 -0300

    Introdução classes estáticas

---
+++ BackEnd/C#_cSharp/orientacao-objeto/classes-estaticas/Program.cs
@@ -0,0 +1,7 @@
+﻿using classes_estaticas;
+
+float resultado1 = ConversorTemperatura.ConverterCelsiusFahrenheit(0);
+float resultado2 = ConversorTemperatura.ConverterFahrenheitCelsius(32);
+
+Console.WriteLine($"{resultado1}");
+Console.WriteLine($"{resultado2}");
