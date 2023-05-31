f2545ed307113a3ee3fd234c919538f71544d97f
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-05-31 17:22:59 -0300

    MVC Cadastrar continuado

--- BackEnd/MVC/projeto-gamer/Controllers/EquipeController.cs
+++ BackEnd/MVC/projeto-gamer/Controllers/EquipeController.cs
@@ -27,21 +27,49 @@ namespace projeto_gamer.Controllers
             return View();
         }
 
-
+        [Route("Cadastrar")]
         public IActionResult Cadastrar(IFormCollection form)
         {
             Equipe novaEquipe = new Equipe();
 
             novaEquipe.Nome = form["Nome"].ToString();
-            novaEquipe.Nome = form["Imagem"].ToString();
+
+            //Vem como string
+            // novaEquipe.Imagem = form["Imagem"].ToString();
+
+            //Aqui começa a lógica do upload de imagem
+            if (form.Files.Count > 0)
+            {
+                //Armazena o arquivo da imagem
+                var file = form.Files[0];
+                //Encontra a pasta do projeto
+                var folder = Path.Combine(Directory.GetCurrentDirectory());
+                //Verifica se a pasta existe
+                if (!Directory.Exists(folder))
+                {
+                    //Cria a pasta
+                    Directory.CreateDirectory(folder);
+                }
+                //
+                var path = Path.Combine(folder, file.FileName);
+                //
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    //Copia para o caminho
+                    file.CopyTo(stream);
+                }
+
+                novaEquipe.Imagem = file.FileName;
+            }
+            else
+            {
+                novaEquipe.Imagem = "padrao.png";
+            }
 
             c.Equipe.Add(novaEquipe);
-
-            c.Add(novaEquipe);
+            //c.Add(novaEquipe);
 
             c.SaveChanges();
-
-            ViewBag.Equipe = c.Equipe.ToList();
 
             return LocalRedirect("~/Equipe/Listar");
         }
